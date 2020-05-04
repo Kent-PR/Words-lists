38535587f4b01a0dcf866192fd6f96dd59fc46fc
KentPR <Promaster081@gmail.com>
2020-05-04 22:42:09 +0300

    Working random

--- Pervak2sem/Source.c
+++ Pervak2sem/Source.c
@@ -53,10 +53,10 @@ struct list* addelem_finite(char* word2, int sym_cnt2)
 {
 	struct finite* lst;
 
-	lst = (struct finite*)malloc(sizeof(struct finite));
+	lst = /*(struct finite*)*/malloc(sizeof(struct finite) + sizeof(int*) * sym_cnt2);
 	lst->N_to = sym_cnt2;
 	//
-	lst->inscription_to = (int*)malloc(sym_cnt2 * sizeof(int));
+	lst->inscription_to = malloc(sym_cnt2 * sizeof(int));
 	//memcpy(word, word2, sym_cnt2); // копирует (sym_cnt2) символов из word2 в поле данных "inscription_to"
 	for (int i = 0; i < sym_cnt2; i++)
 	{
@@ -71,7 +71,7 @@ void addelem_initial(int number, char* word1, int sym_cnt1, char* word2, int sym
 	struct initial* lst;
 	if (!number) // если номер = 0 (то есть список не создан), то создаём первый элемент списка и присваиваем ему "статус" корня
 	{
-		lst = (struct initial*)malloc(sizeof(struct initial)); // выделение памяти под корень списка
+		lst = /*(struct initial*)*/malloc(sizeof(struct initial) + sizeof(int*) * sym_cnt1); // выделение памяти под корень списка
 		ROOT = lst;
 	}
 	else
@@ -79,7 +79,7 @@ void addelem_initial(int number, char* word1, int sym_cnt1, char* word2, int sym
 		lst = HEAD;
 		struct initial* tmp;
 
-		tmp = (struct initial*)malloc(sizeof(struct initial));
+		tmp = /*(struct initial*)*/malloc(sizeof(struct initial));
 		lst->next = tmp; // связываем созданный только что узел с "головой состава"
 		lst = lst->next; // переход на этот самый узел
 		free(tmp);
@@ -89,7 +89,7 @@ void addelem_initial(int number, char* word1, int sym_cnt1, char* word2, int sym
 	lst->num = number;
 	lst->N_from = sym_cnt1;
 	//
-	lst->inscription_from = (int*)malloc(sym_cnt1 * sizeof(int));
+	lst->inscription_from = malloc(sym_cnt1 * sizeof(int));
 	for (int i = 0; i < sym_cnt1; i++)
 	{
 		*((lst->inscription_from)+i) = (int)word1[i];
@@ -139,18 +139,15 @@ void listprint(void)
 	struct initial* lst = ROOT;
 	while (lst != NULL)
 	{
-		int* word1 = lst->inscription_from; //указатель на массив, в котором хранится первое слово (которое заменяют)
 		for (int i = 0; i < lst->N_from; i++)
 		{
-			char c = (char)word1[i];
-			printf("%c", c);
+			printf("%c", (char)*((lst->inscription_from) + i));
 		}
 		printf(" changes to ");
 
-		int* word2 = lst->replacement->inscription_to; //указатель на массив, в котором хранится второе слово (на которое заменяют)
 		for (int i = 0; i < lst->replacement->N_to; i++)
 		{
-			printf("%c", word2[i]);
+			printf("%c", (char)*((lst->replacement->inscription_to)+i));
 		}
 		printf("\n");
 
